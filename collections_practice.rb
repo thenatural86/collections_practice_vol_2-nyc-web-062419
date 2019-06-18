131df81680530d0b56476a740bb099462ee4f6e4
The Natural <philip.smalls86@gmail.com>
2019-06-18 20:28:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,7 +49,7 @@ def organize_schools(schools)
     schools.each do |key, value|
       value.each do |k, v|
         if location[v] == nil
-          location[v] = key
+          location[v] = [key]
         else 
           location[v] << key
         end
